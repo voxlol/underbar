a7204cf4163333ab99dd7989ffb086650c309b04
Allen Chang <iafayte@gmail.com>
2015-05-23 14:46:07 -0500

    Reject tests working

--- src/underbar.js
+++ src/underbar.js
@@ -92,6 +92,9 @@
   _.reject = function(collection, test) {
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
+    return _.filter(collection, function(item){
+      if(!test(item)) return true;
+    })
   };
 
   // Produce a duplicate-free version of the array.
